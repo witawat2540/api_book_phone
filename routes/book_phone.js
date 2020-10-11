05ce62b1d34e23ff34a7fac530cd3a5a755236ad
witawatd <witawatd@gmail.com>
2020-10-11 16:15:53 +0700

    set api

--- routes/book_phone.js
+++ routes/book_phone.js
@@ -1,9 +1,45 @@
-const {
-    Op
-} = require('sequelize');
-const book_phone = require('./model/model_book_phone');
+// const { Op } = require("sequelize");
+const sequelize = require("./config").sequelize;
+const book_phone = require("./model/model_book_phone");
 
+exports.All = async(req, res, next) => {
+    res.send(await book_phone.findAll());
+};
 
-exports.All = (req, res, next) => {
-    res.send("Hello Word");
+exports.Add_book = async(req, res, next) => {
+    res.send(await book_phone.create(req.body));
+};
+
+exports.Update_book = async(req, res, next) => {
+    res.send(await book_phone.update(req.body, { where: { id: req.params.id } }));
+};
+
+exports.bookDelete = async(req, res, next) => {
+    res.json(
+        await book_phone.destroy({
+            where: {
+                id: req.params.id,
+            },
+        })
+    );
+};
+
+exports.bookSearch = async(req, res, next) => {
+    res.send(
+        // await book_phone.findAll({
+        //     where: {
+        //         [Op.or]: [{
+        //             nickname: {
+        //                 [Op.like]: "%" + req.body.keyword + "%",
+        //             },
+        //         }, ],
+        //         [Op.or]: [{
+        //             tel: {
+        //                 [Op.like]: "%" + req.body.keyword + "%",
+        //             },
+        //         }, ],
+        //     },
+        // })
+        await sequelize.query("SELECT * FORM phons")
+    );
 };
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ var router = express.Router();
 var book_phone = require("./book_phone");
 
 router.get('/get_All', book_phone.All);
+router.post('/add_book', book_phone.Add_book);
+router.put('/update_book/:id', book_phone.Update_book);
+router.delete('/delete_book/:id', book_phone.bookDelete);
+router.get('/Search_book/', book_phone.bookSearch);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
